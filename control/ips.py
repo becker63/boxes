de01e33254060d63dfd15821ccb70d9a46ff7434
becker <becker@becker.beckerkali>
2022-01-30 21:48:41 -0500

    first

---
+++ control/ips.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+from netaddr import IPNetwork
+
+for ip in IPNetwork("192.168.4.0/24"):
+	print(ip)
---
+++ control/req.sql
@@ -0,0 +1,16 @@
+
+POST /search_products.php HTTP/1.1
+Host: 10.10.10.167
+User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+Accept-Language: en-US,en;q=0.5
+Accept-Encoding: gzip, deflate
+Content-Type: application/x-www-form-urlencoded
+Content-Length: 109
+Origin: http://10.10.10.167
+X-Forwarded-For: 192.168.4.28
+Connection: close
+Referer: http://10.10.10.167/admin.php
+Upgrade-Insecure-Requests: 1
+
+productName=p
